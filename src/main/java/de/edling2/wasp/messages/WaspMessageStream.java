f46574d6c9cddf440b29ae3ded1bfcefdc548597
Michael Goth <iqe@gmx.net>
2013-06-03 19:31:45 +0200

    Add a combined input/output WaspMessageStream

---
+++ src/main/java/de/edling2/wasp/messages/WaspMessageStream.java
@@ -0,0 +1,49 @@
+package de.edling2.wasp.messages;
+
+import de.edling2.wasp.comm.WaspInputStream;
+import de.edling2.wasp.comm.WaspOutputStream;
+import java.io.IOException;
+
+public class WaspMessageStream {
+	private WaspInputStream input;
+	private WaspOutputStream output;
+	private WaspMessageFactory messageFactory;
+	private byte[] buffer;
+
+	public WaspMessageStream(WaspInputStream input, WaspOutputStream output, String sourcePrefix) {
+		this(input, new byte[1024], output, sourcePrefix);
+	}
+
+	public WaspMessageStream(WaspInputStream input, byte[] inputBuffer, WaspOutputStream output, String sourcePrefix) {
+		this(input, inputBuffer, output, new WaspMessageFactory(sourcePrefix));
+	}
+
+	public WaspMessageStream(WaspInputStream input, byte[] inputBuffer, WaspOutputStream output, WaspMessageFactory messageFactory) {
+		this.input = input;
+		this.output = output;
+		this.buffer = inputBuffer;
+		this.messageFactory = messageFactory;
+	}
+
+	public String getSourcePrefix() {
+		return messageFactory.getSourcePrefix();
+	}
+
+	public void setSourcePrefix(String sourcePrefix) {
+		messageFactory.setSourcePrefix(sourcePrefix);
+	}
+
+	public WaspMessage readMessage() throws IOException {
+		int length = input.readMessageIntoBuffer(buffer);
+		return messageFactory.buildMessage(buffer, length);
+	}
+
+	public void writeMessage(WaspMessage message) throws IOException {
+		byte[] bytes = messageFactory.buildMessageBytes(message);
+		output.writeMessage(bytes);
+	}
+
+	public boolean handlesMessage(WaspMessage message) {
+		return messageFactory.handlesMessage(message);
+	}
+}
