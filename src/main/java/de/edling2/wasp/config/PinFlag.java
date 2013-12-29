6b5810931f0a206ce556a2f72ce19b7063634d71
Michael Goth <iqe@gmx.net>
2013-12-29 23:00:22 +0100

    Add bitmask value and helper method to PinFlag

--- src/main/java/de/edling2/wasp/config/PinFlag.java
+++ src/main/java/de/edling2/wasp/config/PinFlag.java
@@ -1,6 +1,30 @@
 package de.edling2.wasp.config;
 
+import java.util.EnumSet;
+
 public enum PinFlag {
-	InputPullup,
-	Reversed
+	InputPullup(1),
+	Reversed(2);
+
+	public static EnumSet<PinFlag> fromBitField(int flags) {
+		EnumSet<PinFlag> set = EnumSet.noneOf(PinFlag.class);
+
+		for (PinFlag flag : PinFlag.values()) {
+			if ((flags & flag.getValue()) != 0) {
+				set.add(flag);
+			}
+		}
+
+		return set;
+	}
+
+	private int value;
+
+	private PinFlag(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
 }
